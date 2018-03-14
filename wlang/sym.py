3d82df8d9e8a7a7a49ee9f40c3f3194d12f7e629
mnsamad <35379100+mnsamad@users.noreply.github.com>
2018-03-14 18:44:56 -0400

    update q4 files

--- wlang/sym.py
+++ wlang/sym.py
@@ -22,11 +22,14 @@
 
 from __future__ import print_function
 
+from wlang.undef_visitor import UndefVisitor
 import wlang.ast
 import cStringIO
 import sys
 
 import z3
+import wlang.undef_visitor as undef_visitor
+import wlang.ast as ast
 
 class SymState(object):
     def __init__(self, solver = None):
@@ -99,13 +102,13 @@ class SymState(object):
         return buf.getvalue ()
                    
 class SymExec (wlang.ast.AstVisitor):
-    def __init__(self):
-        pass
+    def __init__(self, loop_bound=10):
+        self._global_loop_bound = loop_bound
 
     def run (self, ast, state):
-        ## set things up and 
-        ## call self.visit (ast, state=state)
-        pass
+        if not state.is_empty ():
+            for out in self.visit (ast, state=state):
+                yield out
 
     def visit_IntVar (self, node, *args, **kwargs):
         return kwargs['state'].env [node.name]
@@ -117,47 +120,213 @@ class SymExec (wlang.ast.AstVisitor):
         return z3.IntVal (node.val)
     
     def visit_RelExp (self, node, *args, **kwargs):
-        pass
+        lhs = self.visit (node.arg (0), *args, **kwargs)
+        rhs = self.visit (node.arg (1), *args, **kwargs)
+        if node.op == '<=': return lhs <= rhs
+        if node.op == '<': return lhs < rhs
+        if node.op == '=': return lhs == rhs
+        if node.op == '>=': return lhs >= rhs
+        if node.op == '>': return lhs > rhs
+        
+        assert False
 
     def visit_BExp (self, node, *args, **kwargs):
-        pass
+        kids = [self.visit (a, *args, **kwargs) for a in node.args]
+        
+        if node.op == 'not':
+            assert node.is_unary ()
+            assert len (kids) == 1
+            return z3.Not (kids[0])
+        
+        fn = None
+        base = None
+        if node.op == 'and':
+            fn = lambda x, y : z3.And (x, y)
+            base = z3.BoolVal (True)
+        elif node.op == 'or':
+            fn = lambda x, y : z3.Or (x, y)
+            base = z3.BoolVal (False)
+
+        assert fn is not None
+        return reduce (fn, kids, base)
         
     def visit_AExp (self, node, *args, **kwargs):
-        pass
+        kids = [self.visit (a, *args, **kwargs) for a in node.args]
+
+        fn = None
+        base = None
+
+        if node.op == '+':
+            fn = lambda x, y: x + y
+            
+        elif node.op == '-':
+            fn = lambda x, y: x - y
+
+        elif node.op == '*':
+            fn = lambda x, y: x * y
+
+        elif node.op == '/':
+            fn = lambda x, y : x / y
+            
+        
+        assert fn is not None
+        return reduce (fn, kids)
         
     def visit_SkipStmt (self, node, *args, **kwargs):
-        pass
+        yield kwargs['state']
     
     def visit_PrintStateStmt (self, node, *args, **kwargs):
-        pass
+        print (kwargs['state'])
+        yield kwargs['state']
 
     def visit_AsgnStmt (self, node, *args, **kwargs):
-        pass
+        val = self.visit (node.rhs, *args, **kwargs)
+        
+        st = kwargs['state']
+        name = node.lhs.name
+        sym_val = z3.FreshInt (name)
+        st.env [name] = sym_val
+        st.add_pc (sym_val == val)
+        yield st
 
     def visit_IfStmt (self, node, *args, **kwargs):
-        pass
+        cond_val = self.visit (node.cond, *args, **kwargs)
+
+        st = kwargs['state']
+        then_branch, else_branch = st.fork ()
+        then_branch.add_pc (cond_val)
+        
+        if not then_branch.is_empty ():
+            # cover all path under then branch
+            nkwargs = dict (kwargs)
+            nkwargs['state'] = then_branch
+            for out in self.visit (node.then_stmt, *args, **nkwargs):
+                yield out
+            
+        else_branch.add_pc (z3.Not (cond_val))
+        if not else_branch.is_empty ():
+            if node.has_else ():
+                nkwargs = dict (kwargs)
+                nkwargs['state'] = else_branch
+                for out in self.visit (node.else_stmt, *args, **nkwargs):
+                    yield out
+            else:        
+                yield else_branch
             
     def visit_WhileStmt (self, node, *args, **kwargs):
-        pass
+        """ Symbolic execution of while loops """
+        if node.inv is not None:
+            for out in self.visit_WhileStmt_inv (node, *args, **kwargs):
+                yield out
+        else:
+            for out in self.visit_WhileStmt_noinv (node, *args, **kwargs):
+                yield out
+                
+    def visit_WhileStmt_inv (self, node, *args, **kwargs):
+        """" Symbolic execution of while loops with invariants """
+        uv = undef_visitor.UndefVisitor()
+        uv.check (node.body);
+        vars = uv.get_undefs();
+        c = node.inv.args[0];
+        vars = list(vars);
+        st = kwargs['state']
+        for i in node.body.stmts:
+            c = i;
+
+        prg = "assert "+str(node.inv)+ ";havoc " + str(vars[0])+ ","+str(vars[1])+ "; assume " + str(node.inv.args[0]) + "; if "+ str(node.cond) + "then {"+ str(node.body.stmts[0])+";"+str(node.body.stmts[1]) +";assert "+ str(node.inv)+"; assume false}" ;
+        #prg = "assert "+str(node.inv.args[0]);
+        ast1 = ast.parse_string (prg);
+        for i in  self._run_Stmts(ast1.stmts,st):
+            yield i
+
+            
+    def visit_WhileStmt_noinv (self, node, *args, **kwargs):
+        """ Symbolic execution of while loops with no invariants """
+        bound = kwargs.get ('loop_bound')
+        if bound is None:
+            bound = self._global_loop_bound
+            
+        cond_val = self.visit (node.cond, *args, **kwargs);
+        
+        # one state enters the loop, one exits
+        enter_st, exit_st = kwargs['state'].fork ()
+
+        # if enter loop, loop condition is true
+        enter_st.add_pc (cond_val)
+        # if exit loop, loop condition is false
+        exit_st.add_pc (z3.Not (cond_val))
+        
+        # if loop condition can be satisfied and we have not tripped loop bound
+        if bound > 0 and not enter_st.is_empty ():
+            # do loop body, might produce many new states
+            for out in self.visit (node.body, *args, state=enter_st):
+                for out2 in self.visit (node, *args, state=out, loop_bound=bound - 1):
+                    yield out2
+
+        # if negation of loop condition can be satisfied then can exit
+        # the loop immediatelly
+        if not exit_st.is_empty ():
+            yield exit_st
+            
 
     def visit_AssertStmt (self, node, *args, **kwargs):
-        ## Don't forget to print an error message if an assertion might be violated
-        pass
+        st = kwargs['state']
+        cond_val = self.visit (node.cond, *args, **kwargs)
+        true_state, false_state = st.fork ()
+        false_state.add_pc (z3.Not (cond_val))
+        if not false_state.is_empty ():
+            print ('[symexec]: Error at', node, 'with', false_state)
+            print ('[symexec]: Concrete state', false_state.pick_concerete())
+            false_state.mk_error ()
+            
+        true_state.add_pc (cond_val)
+        if not true_state.is_empty ():
+            yield true_state
     
     def visit_AssumeStmt (self, node, *args, **kwargs):
-        pass
+        st = kwargs['state']
+        cond_val = self.visit (node.cond, *args, **kwargs)
+        st.add_pc (cond_val)
+        if not st.is_empty ():
+            yield st
 
     def visit_HavocStmt (self, node, *args, **kwargs):
-        pass
+        st = kwargs['state']
+        for v in node.vars:
+            st.env[v.name] = z3.FreshInt (v.name)
+        yield st
 
     def visit_StmtList (self, node, *args, **kwargs):
-        pass
+        st = kwargs['state']
+        for out in self._run_Stmts (node.stmts, st):
+            yield out
         
+    def _run_Stmts (self, stmts, st):
+        """Recursively run a sequential list of statements"""
+        
+        # empty sequence is just like a skip
+        if len (stmts) == 0:
+            yield st
+            
+        # a single statement is simply executed
+        elif len (stmts) == 1:
+            for out in self.run (stmts [0], st):
+                yield out
+
+        # for every output state of executing the first statement,
+        # execute the rest of the sequence
+        else:
+            for out1 in self.run (stmts [0], st):
+                for out2 in self._run_Stmts (stmts[1:], out1):
+                    yield out2
+    
 def _parse_args ():
     import argparse
     ap = argparse.ArgumentParser (prog='sym',
                                   description='WLang Interpreter')
     ap.add_argument ('in_file', metavar='FILE', help='WLang program to interpret')
+    ap.add_argument ('--bound', metavar='BOUND', help='Global loop bound', \
+                     type=int, default=10)
     args = ap.parse_args ()
     return args
     
@@ -165,7 +334,7 @@ def main ():
     args = _parse_args ()
     ast = wlang.ast.parse_file (args.in_file)
     st = SymState ()
-    sym = SymExec ()
+    sym = SymExec (loop_bound=args.bound)
 
     states = sym.run (ast, st)
     if states is None:
--- wlang/test_sym.py
+++ wlang/test_sym.py
@@ -26,11 +26,22 @@ import wlang.sym
 
 class TestSym (unittest.TestCase):
     def test_one (self):
-        prg1 = "havoc x; assume x > 10; assert x > 15"
+
+        #prg1 = "havoc x,y; assume y>=0 "
+        prg1 = "havoc x,y; assume y>=0; c:=0; r:=x; while c<y inv (c<=y and r=x+c) do {r:= r+1; c:= c+1}; assert r =x+y"
         ast1 = ast.parse_string (prg1)
         sym = wlang.sym.SymExec ()
         st = wlang.sym.SymState ()
         out = [s for s in sym.run (ast1, st)]
         self.assertEquals (len(out), 1)
-        
+
+    def test_two (self): # For the d part of ques 3
+
+        #prg1 = "havoc x,y; assume y>=0 "
+        prg1 = "havoc x,y; assume y>=0; c:=y; r:=x; while c>0 inv (c<=y and r=x+y-c) do {r:= r+1; c:= c-1}; assert r =x+y"
+        ast1 = ast.parse_string (prg1)
+        sym = wlang.sym.SymExec ()
+        st = wlang.sym.SymState ()
+        out = [s for s in sym.run (ast1, st)]
+        self.assertEquals (len(out), 1)
         
